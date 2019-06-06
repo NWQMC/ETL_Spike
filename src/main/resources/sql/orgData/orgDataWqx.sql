27ceadd45ebf3c4274464d5f561e79fa9bb94a49
David Steinich <drsteini@contractor.usgs.gov>
2019-06-06 13:48:11 -0500

    orgDataWqx.sql

--- src/main/resources/sql/orgData/orgDataWqx.sql
+++ src/main/resources/sql/orgData/orgDataWqx.sql
@@ -1,4 +1,23 @@
-insert /*+ append parallel(4) */
+with addresses as (select org_address."ORG_UID" org_uid,
+                          address_type."ADDTYP_NAME" address_type,
+                          org_address."ORGADD_ADDRESS" address_text,
+                          org_address."ORGADD_ADDRESS_SUPPLEMENTAL" supplemental_address_text,
+                          org_address."ORGADD_LOCALITY_NAME" locality_name,
+                          org_address."ORGADD_POSTAL_CD" postal_code,
+                          country.cntry_cd country_code,
+                          state.st_cd state_code,
+                          county.cnty_fips_cd county_code,
+                          row_number() over (partition by org_address."ORG_UID" order by org_address."ORGADD_UID") addr_num
+                     from wqx."ORG_ADDRESS" org_address
+                          join wqx."ADDRESS_TYPE" address_type
+                            on org_address."ADDTYP_UID" = address_type."ADDTYP_UID"
+                          left join wqx.country
+                            on org_address."CNTRY_UID" = country.cntry_uid
+                          left join wqx.state
+                            on org_address."ST_UID" = state.st_uid
+                          left join wqx.county
+                            on org_address."CNTY_UID" = county.cnty_uid)
+insert
   into org_data_swap_storet (data_source_id, data_source, organization_id, organization, organization_name,
                              organization_description, organization_type, tribal_code, electronic_address, telephonic, address_type_1,
                              address_text_1, supplemental_address_text_1, locality_name_1, postal_code_1,
@@ -7,91 +26,68 @@ insert /*+ append parallel(4) */
                              state_code_2, county_code_2, address_type_3, address_text_3,
                              supplemental_address_text_3, locality_name_3, postal_code_3, country_code_3,
                              state_code_3, county_code_3)
-select /*+ parallel(4) */ 
-       3 data_source_id,
+select 3 data_source_id,
        'STORET' data_source,
-       organization.org_uid organization_id,
-       organization.org_id organization,
-       organization.org_name organization_name,
-       organization.org_desc organization_description,
-       organization.org_type organization_type,
-       tribe.trb_name tribal_code,
+       organization."ORG_UID" organization_id,
+       organization."ORG_ID" organization,
+       organization."ORG_NAME" organization_name,
+       organization."ORG_DESC" organization_description,
+       organization."ORG_TYPE" organization_type,
+       tribe."TRB_NAME" tribal_code,
        org_electronic_address.electronic_address,
        org_phone.telephonic,
-       org_address.a_addtyp_name address_type_1,
-       org_address.a_orgadd_address address_text_1,
-       org_address.a_orgadd_address_supplemental supplemental_address_text_1,
-       org_address.a_orgadd_locality_name locality_name_1,
-       org_address.a_orgadd_postal_cd postal_code_1,
-       org_address.a_cntry_cd country_code_1,
-       org_address.a_st_cd state_code_1,
-       org_address.a_cnty_fips_cd county_code_1,
-       org_address.b_addtyp_name address_type_2,
-       org_address.b_orgadd_address address_text_2,
-       org_address.b_orgadd_address_supplemental supplemental_address_text_2,
-       org_address.b_orgadd_locality_name locality_name_2,
-       org_address.b_orgadd_postal_cd postal_code_2,
-       org_address.b_cntry_cd country_code_2,
-       org_address.b_st_cd state_code_2,
-       org_address.b_cnty_fips_cd county_code_2,
-       org_address.c_addtyp_name address_type_3,
-       org_address.c_orgadd_address address_text_3,
-       org_address.c_orgadd_address_supplemental supplemental_address_text_3,
-       org_address.c_orgadd_locality_name locality_name_3,
-       org_address.c_orgadd_postal_cd postal_code_3,
-       org_address.c_cntry_cd country_code_3,
-       org_address.c_st_cd state_code_3,
-       org_address.c_cnty_fips_cd county_code_3
-  from wqx.organization
-       left join wqx.tribe
-         on organization.trb_uid = tribe.trb_uid
-       left join (select org_uid,
-                         listagg(orgea_text || ' (' || eatyp_name || ')', ';') within group (order by orgea_uid) electronic_address
-                    from wqx.org_electronic_address
-                         join wqx.electronic_address_type
-                           on org_electronic_address.eatyp_uid = electronic_address_type.eatyp_uid
+       addresses1.address_type address_type_1,
+       addresses1.address_text address_text_1,
+       addresses1.supplemental_address_text supplemental_address_text_1,
+       addresses1.locality_name locality_name_1,
+       addresses1.postal_code postal_code_1,
+       addresses1.country_code country_code_1,
+       addresses1.state_code state_code_1,
+       addresses1.county_code county_code_1,
+       addresses2.address_type address_type_2,
+       addresses2.address_text address_text_2,
+       addresses2.supplemental_address_text supplemental_address_text_2,
+       addresses2.locality_name locality_name_2,
+       addresses2.postal_code postal_code_2,
+       addresses2.country_code country_code_2,
+       addresses2.state_code state_code_2,
+       addresses2.county_code county_code_2,
+       addresses3.address_type address_type_3,
+       addresses3.address_text address_text_3,
+       addresses3.supplemental_address_text supplemental_address_text_3,
+       addresses3.locality_name locality_name_3,
+       addresses3.postal_code postal_code_3,
+       addresses3.country_code country_code_3,
+       addresses3.state_code state_code_3,
+       addresses3.county_code county_code_3
+  from wqx."ORGANIZATION" organization
+       left join wqx."TRIBE" tribe
+         on organization."TRB_UID" = tribe."TRB_UID"
+       left join (select "ORG_UID" org_uid,
+                         string_agg("ORGEA_TEXT" || ' (' || "EATYP_NAME" || ')', ';' order by "ORGEA_UID") electronic_address
+                    from wqx."ORG_ELECTRONIC_ADDRESS" org_electronic_address
+                         join wqx."ELECTRONIC_ADDRESS_TYPE" electronic_address_type
+                           on org_electronic_address."EATYP_UID" = electronic_address_type."EATYP_UID"
                       group by org_uid) org_electronic_address
-         on organization.org_uid = org_electronic_address.org_uid
-       left join (select org_uid,
-                         listagg(orgph_num ||
-                                   case when orgph_ext is not null then ' x' || orgph_ext end || 
-                                   ' (' || phtyp_name || ')', ';')
-                             within group (order by orgph_uid) telephonic
-                    from wqx.org_phone
-                         join wqx.phone_type
-                           on org_phone.phtyp_uid = phone_type.phtyp_uid
+         on organization."ORG_UID" = org_electronic_address.org_uid
+       left join (select "ORG_UID" org_uid,
+                         string_agg("ORGPH_NUM" ||
+                                   case when "ORGPH_EXT" is not null then ' x' || "ORGPH_EXT" end || 
+                                   ' (' || "PHTYP_NAME" || ')', ';'
+                             order by "ORGPH_UID") telephonic
+                    from wqx."ORG_PHONE" org_phone
+                         join wqx."PHONE_TYPE" phone_type
+                           on org_phone."PHTYP_UID" = phone_type."PHTYP_UID"
                       group by org_uid) org_phone
-         on organization.org_uid = org_phone.org_uid
-       left join (select *
-                    from (select org_address.org_uid,
-                                 address_type.addtyp_name,
-                                 org_address.orgadd_address,
-                                 org_address.orgadd_address_supplemental,
-                                 org_address.orgadd_locality_name,
-                                 org_address.orgadd_postal_cd,
-                                 country.cntry_cd,
-                                 state.st_cd,
-                                 county.cnty_fips_cd,
-                                 row_number() over (partition by org_address.org_uid order by org_address.orgadd_uid) addr_num
-                            from wqx.org_address
-                                 join wqx.address_type
-                                   on org_address.addtyp_uid = address_type.addtyp_uid
-                                 left join wqx.country
-                                   on org_address.cntry_uid = country.cntry_uid
-                                 left join wqx.state
-                                   on org_address.st_uid = state.st_uid
-                                 left join wqx.county
-                                   on org_address.cnty_uid = county.cnty_uid)
-                   pivot (min(addtyp_name) addtyp_name,
-                          min(orgadd_address) orgadd_address,
-                          min(orgadd_address_supplemental) orgadd_address_supplemental,
-                          min(orgadd_locality_name) orgadd_locality_name,
-                          min(orgadd_postal_cd) orgadd_postal_cd,
-                          min(cntry_cd) cntry_cd,
-                          min(st_cd) st_cd,
-                          min(cnty_fips_cd) cnty_fips_cd
-                           for (addr_num) in (1 a, 2 b, 3 c)
-                         )
-                 ) org_address
+         on organization."ORG_UID" = org_phone.org_uid
+       left join addresses addresses1
+          on organization."ORG_UID" = addresses1.org_uid and
+             1 = addresses1.addr_num
+        left join addresses addresses2
+          on organization."ORG_UID" = addresses2.org_uid and
+             2 = addresses2.addr_num
+        left join addresses addresses3
+          on organization."ORG_UID" = addresses3.org_uid and
+             3 = addresses3.addr_num
          on organization.org_uid = org_address.org_uid
- where organization.org_uid not between 2000 and 2999
+ where organization."ORG_UID" not between 2000 and 2999
--- src/test/resources/db/todo.sql
+++ src/test/resources/db/todo.sql
@@ -1,19 +1,53 @@
+create extension tablefunc;
+
 create role storet_owner with login password 'changeMe';
 grant storet_owner to postgres;
 create schema wqx authorization storet_owner;
-
 alter default privileges in schema wqx grant select on tables to wqp_core;
 grant usage on schema wqx to wqp_core;
 
+create or replace view wqx.country as
+select distinct
+       "CNTRY_UID" cntry_uid,
+       "CNTRY_CD" cntry_cd,
+       "CNTRY_NAME" cntry_name,
+       "CNTRY_LAST_CHANGE_DATE" cntry_last_change_date,
+       "USR_UID_LAST_CHANGE" usr_uid_last_change
+  from wqx."COUNTRY";
+
+create or replace view wqx.state as
+select distinct
+       "ST_UID" st_uid,
+       "CNTRY_UID" cntry_uid,
+       "ST_CD" st_cd,
+       "ST_NAME" st_name,
+       "ST_FIPS_CD" st_fips_cd,
+       "ST_LAST_CHANGE_DATE" st_last_change_date,
+       "ER_UID" er_uid,
+       "USR_UID_LAST_CHANGE" usr_uid_last_change
+  from wqx."STATE";
+
+create or replace view wqx.county as
+select distinct
+       "CNTY_UID" cnty_uid,
+       "ST_UID" st_uid,
+       "CNTY_NAME" cnty_name,
+       "CNTY_FIPS_CD" cnty_fips_cd,
+       "CNTY_LAST_CHANGE_DATE" cnty_last_change_date,
+       "USR_UID_LAST_CHANGE" usr_uid_last_change
+  from wqx."COUNTY";
+
 create or replace view wqx.full_country (country_code, country_name) as
-select coalesce(nwis.country_cd, wqx."CNTRY_CD") country_code,
+select distinct
+       coalesce(nwis.country_cd, wqx."CNTRY_CD") country_code,
        coalesce(nwis.country_nm, wqx."CNTRY_NAME") country_name
   from nwis.country nwis
        full outer join wqx."COUNTRY" wqx
          on nwis.country_cd = wqx."CNTRY_CD";
          
 create or replace view wqx.full_county (county_code, county_name) as
-select coalesce(nwis.country_cd, wqx.cntry_cd) || ':' ||
+select distinct
+       coalesce(nwis.country_cd, wqx.cntry_cd) || ':' ||
        coalesce(nwis.state_cd, wqx.st_fips_cd) || ':' ||
        coalesce(nwis.county_cd, wqx.cnty_fips_cd) county_code,
        coalesce(nwis.county_nm, wqx.cnty_name) county_name
@@ -34,7 +68,8 @@ select coalesce(nwis.country_cd, wqx.cntry_cd) || ':' ||
             nwis.county_cd = wqx.cnty_fips_cd;
             
 create or replace view wqx.full_state (state_code, state_name) as
-select coalesce(nwis.country_cd, wqx.cntry_cd) || ':' ||
+select distinct
+       coalesce(nwis.country_cd, wqx.cntry_cd) || ':' ||
        coalesce(nwis.state_cd, wqx.st_fips_cd) state_code,
        coalesce(nwis.state_nm, wqx.st_name) state_name
   from nwis.state nwis
