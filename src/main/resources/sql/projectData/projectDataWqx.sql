a24c9dcb55ca9beebfceafbc6cf7174738347966
David Steinich <drsteini@contractor.usgs.gov>
2019-06-06 14:54:01 -0500

    projectDataWqx

--- src/main/resources/sql/projectData/projectDataWqx.sql
+++ src/main/resources/sql/projectData/projectDataWqx.sql
@@ -1,4 +1,4 @@
-insert /*+ append parallel(4) */
+insert
   into project_data_swap_storet (data_source_id,
                                  project_id,
                                  data_source,
@@ -9,43 +9,43 @@ insert /*+ append parallel(4) */
                                  description,
                                  sampling_design_type_code,
                                  qapp_approved_indicator,
-                                 qapp_approval_agency_name,
-                                 project_file_url,
-                                 monitoring_location_weight_url
+                                 qapp_approval_agency_name--,
+--                                 project_file_url,
+--                                 monitoring_location_weight_url
                                 )
 select 3 data_source_id,
-       project.prj_uid project_id,
+       project."PRJ_UID" project_id,
        'STORET' data_source,
-       organization.org_id organization,
-       organization.org_name organization_name,
-       project.prj_id project_identifier,
-       project.prj_name project_name,
-       project.prj_desc description,
-       sampling_design_type.sdtyp_desc sampling_design_type_code,
-       project.prj_qapp_approved_yn qapp_approved_indicator,
-       project.prj_qapp_approval_agency_name qapp_approval_agency_name,
-       case 
-         when attached_object.has_blob is not null
-           then '/organizations/' || pkg_dynamic_list.url_escape(organization.org_id, 'true') || '/projects/' || pkg_dynamic_list.url_escape(project.prj_id, 'true') || '/files'
-         else null
-       end project_file_url,
-       case
-         when monitoring_location_weight.has_weight is not null
-           then '/organizations/' || pkg_dynamic_list.url_escape(organization.org_id, 'true') || '/projects/' || pkg_dynamic_list.url_escape(project.prj_id, 'true') || '/projectMonitoringLocationWeightings'
-         else null
-       end monitoring_location_weight_url
-  from wqx.project
-       join wqx.organization
-         on project.org_uid = organization.org_uid
-       left join wqx.sampling_design_type
-         on project.sdtyp_uid = sampling_design_type.sdtyp_uid
-       left join (select org_uid, ref_uid, count(*) has_blob
-                    from wqx.attached_object
-                   where 1 = tbl_uid
-                     group by org_uid, ref_uid) attached_object
-         on project.org_uid = attached_object.org_uid and
-            project.prj_uid = attached_object.ref_uid
-       left join (select prj_uid, count(*) has_weight
-                    from wqx.monitoring_location_weight
+       organization."ORG_ID" organization,
+       organization."ORG_NAME" organization_name,
+       project."PRJ_ID" project_identifier,
+       project."PRJ_NAME" project_name,
+       project."PRJ_DESC" description,
+       sampling_design_type."SDTYP_DESC" sampling_design_type_code,
+       project."PRJ_QAPP_APPROVED_YN" qapp_approved_indicator,
+       project."PRJ_QAPP_APPROVAL_AGENCY_NAME" qapp_approval_agency_name--,
+--       case 
+--         when attached_object.has_blob is not null
+--           then '/organizations/' || pkg_dynamic_list.url_escape(organization.org_id, 'true') || '/projects/' || pkg_dynamic_list.url_escape(project.prj_id, 'true') || '/files'
+--         else null
+--       end project_file_url,
+--       case
+--         when monitoring_location_weight.has_weight is not null
+--           then '/organizations/' || pkg_dynamic_list.url_escape(organization."ORG_ID", 'true') || '/projects/' || pkg_dynamic_list.url_escape(project."PRJ_ID", 'true') || '/projectMonitoringLocationWeightings'
+--         else null
+--       end monitoring_location_weight_url
+  from wqx."PROJECT" project
+       join wqx."ORGANIZATION" organization
+         on project."ORG_UID" = organization."ORG_UID"
+       left join wqx."SAMPLING_DESIGN_TYPE" sampling_design_type
+         on project."SDTYP_UID" = sampling_design_type."SDTYP_UID"
+--       left join (select org_uid, ref_uid, count(*) has_blob
+--                    from wqx.attached_object
+--                   where 1 = tbl_uid
+--                     group by org_uid, ref_uid) attached_object
+--         on project.org_uid = attached_object.org_uid and
+--            project.prj_uid = attached_object.ref_uid
+       left join (select "PRJ_UID" prj_uid, count(*) has_weight
+                    from wqx."MONITORING_LOCATION_WEIGHT" monitoring_location_weight
                       group by prj_uid) monitoring_location_weight
-         on project.prj_uid = monitoring_location_weight.prj_uid
+         on project."PRJ_UID" = monitoring_location_weight.prj_uid
