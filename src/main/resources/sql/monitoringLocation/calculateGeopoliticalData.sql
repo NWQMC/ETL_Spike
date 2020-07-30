22259e9f95d49d8acd50183f17372c7f3c85133c
David Steinich <drsteini@contractor.usgs.gov>
2020-07-30 12:52:06 -0500

    WQP-1574

--- src/main/resources/sql/monitoringLocation/calculateGeopoliticalData.sql
+++ src/main/resources/sql/monitoringLocation/calculateGeopoliticalData.sql
@@ -1,8 +1,8 @@
 update wqx.monitoring_location_local upd_table
-   set calculated_fips = coalesce(fips_state_code, '') || coalesce(fips_county_code, '')
+   set calculated_fips = geoid
   from wqx.monitoring_location_local src_table
-       join county_geom
-         on st_covers(county_geom.geom, st_transform(src_table.geom, 4326))
+       join tl_2019_us_county_geopkg
+         on st_covers(tl_2019_us_county_geopkg.wkb_geometry, src_table.geom)
  where src_table.cntry_cd in ('AS','PR','UM','US', 'VI') and
        src_table.calculated_fips is null and 
        upd_table.station_id = src_table.station_id and
