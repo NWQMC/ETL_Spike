69073a698866a35a4d98de8be3e7a7a37ec750c5
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-08-31 15:48:25 -0700

    clean up sql

--- src/main/resources/sql/result/resultStoretw.sql
+++ src/main/resources/sql/result/resultStoretw.sql
@@ -56,7 +56,7 @@ select 3 data_source_id,
                result_no_source.sample_fraction_type,
                result_no_source.result_measure_value,
                result_no_source.result_unit,
-               string_agg(result_no_source.result_meas_qual_code, ';') over (partition by result_no_source.result_meas_qual_code) result_meas_qual_code,
+               result_no_source.result_meas_qual_code,
                result_no_source.result_value_status,
                result_no_source.statistic_type,
                result_no_source.result_value_type,
@@ -85,38 +85,47 @@ select 3 data_source_id,
                  on result_no_source.station_id + 10000000 = station.station_id) a;
 
 update result_swap_storet
-  set result_meas_qual_code=subquery.result_meas_qual_code
+  set result_meas_qual_code=t2.result_meas_qual_code
   from
   (select
      activity, sample_media, characteristic_name, event_date, data_source_id, station_id, site_id,
      analytical_method, characteristic_type, site_type, huc, governmental_unit_code, geom,
-     string_agg(result_meas_qual_code, ';') as result_meas_qual_code
+     string_agg(result_meas_qual_code, ';' order by result_meas_qual_code) as result_meas_qual_code
      from result_swap_storet
      group by activity, sample_media, characteristic_name, event_date, data_source_id, station_id, site_id,
        analytical_method, characteristic_type, site_type, huc, governmental_unit_code, geom
-  ) as subquery
-  where result_swap_storet.activity = subquery.activity
-    and result_swap_storet.sample_media = subquery.sample_media
-    and result_swap_storet.characteristic_name = subquery.characteristic_name
-    and result_swap_storet.event_date = subquery.event_date
-    and result_swap_storet.data_source_id = subquery.data_source_id
-    and result_swap_storet.station_id = subquery.station_id
-    and result_swap_storet.site_id = subquery.site_id
-    and result_swap_storet.analytical_method = subquery.analytical_method
-    and result_swap_storet.analytical_method = subquery.analytical_method
-    and result_swap_storet.characteristic_type = subquery.characteristic_type
-    and result_swap_storet.site_type = subquery.site_type
-    and result_swap_storet.huc = subquery.huc
-    and result_swap_storet.governmental_unit_code = subquery.governmental_unit_code
-    and result_swap_storet.geom = subquery.geom;
+  ) as t2
+  where result_swap_storet.activity = t2.activity
+    and result_swap_storet.sample_media = t2.sample_media
+    and result_swap_storet.characteristic_name = t2.characteristic_name
+    and result_swap_storet.event_date = t2.event_date
+    and result_swap_storet.data_source_id = t2.data_source_id
+    and result_swap_storet.station_id = t2.station_id
+    and result_swap_storet.site_id = t2.site_id
+    and result_swap_storet.analytical_method = t2.analytical_method
+    and result_swap_storet.analytical_method = t2.analytical_method
+    and result_swap_storet.characteristic_type = t2.characteristic_type
+    and result_swap_storet.site_type = t2.site_type
+    and result_swap_storet.huc = t2.huc
+    and result_swap_storet.governmental_unit_code = t2.governmental_unit_code
+    and result_swap_storet.geom = t2.geom;
 
-
-
-delete from result_swap_storet T1
-  using result_swap_storet T2
-  where T1.result_id < T2.result_id
-    and T1.activity = T2.activity
-    and T1.sample_media = T2.sample_media
-    and T1.characteristic_name = T2.characteristic_name
-    and T1.event_date = T2.event_date
-    and T1.result_meas_qual_code is not null;
+delete from result_swap_storet t1
+  using result_swap_storet t2
+  where t1.result_id < t2.result_id
+    and t1.activity = t2.activity
+    and t1.sample_media = t2.sample_media
+    and t1.characteristic_name = t2.characteristic_name
+    and t1.event_date = t2.event_date
+    and t1.data_source_id = t2.data_source_id
+    and t1.station_id = t2.station_id
+    and t1.site_id = t2.site_id
+    and t1.analytical_method = t2.analytical_method
+    and t1.analytical_method = t2.analytical_method
+    and t1.characteristic_type = t2.characteristic_type
+    and t1.site_type = t2.site_type
+    and t1.huc = t2.huc
+    and t1.governmental_unit_code = t2.governmental_unit_code
+    and t1.geom = t2.geom
+    and t1.result_meas_qual_code is not null
+    and t2.result_meas_qual_code is not null;
