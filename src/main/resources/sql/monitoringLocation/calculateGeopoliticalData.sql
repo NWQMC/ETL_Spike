5c00445dcfd05df5b933094e660b54326c036911
eorosz <eorosz@contractor.usgs.gov>
2020-10-29 17:19:03 -0400

    WQP-1565 (Potential ETL issue with certain sites)    Updated calculateGeopoliticalData.sql to not check for country code (cntry_cd) in where clause.    Added monitoring location from the issue to wqxDump/csv/ORGANIZATION.csv does not have country code set (null).    Added organizational data from issue to test data.    Added table tl_2019_us_county_geopkg to test data. just contains the county from issue.    Updated EtlEpaIT to use tl_2019_us_county_geopkg table.    Added wqxDump/csv/MONITORING_LOCATION_TYPE to test data.

--- src/main/resources/sql/monitoringLocation/calculateGeopoliticalData.sql
+++ src/main/resources/sql/monitoringLocation/calculateGeopoliticalData.sql
@@ -1,9 +1,8 @@
 update wqx.monitoring_location_local upd_table
-   set calculated_fips = geoid
+   set calculated_fips = geoid, cntry_cd = 'US'
   from wqx.monitoring_location_local src_table
        join tl_2019_us_county_geopkg
          on st_covers(tl_2019_us_county_geopkg.wkb_geometry, src_table.geom)
- where src_table.cntry_cd in ('AS','PR','UM','US', 'VI') and
-       src_table.calculated_fips is null and 
+ where src_table.calculated_fips is null and
        upd_table.station_id = src_table.station_id and
        upd_table.monitoring_location_source = src_table.monitoring_location_source
